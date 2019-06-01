42b202ae93ea80a4c13b475a5d8d90bbd33ff941
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-01 18:14:57 -0400

    linked to jQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//Assign global variables
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+//Generate random number
+var randomNumber = (Math.floor(Math.random() * 101)+ 19)
+
+//Generate random gem values
+var randomGem = (Math.floor(Math.random() * 12) + 1)
+
