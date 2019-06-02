b48af9e4ebca6cba2f8fa52cb3cbbc9fd98eb81b
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-02 00:03:46 -0400

    coded click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,52 @@
 var wins = 0;
 var losses = 0;
 var totalScore = 0;
+var assign = []
 
 //Generate random number
 var randomNumber = (Math.floor(Math.random() * 101)+ 19)
+
 //Display in random-number div
-$("#random-number").html(randomNumber)
+var displayRandom = $("#random-number").text(randomNumber)
+
+//Display total score in total-score div
+var displayScore = $('#total-score').html(totalScore)
 
 //Generate random gem values
 function randomGem () {
-    var assign = []
     for(i = 0; i < 4; i++) {
         assign.push(Math.floor(Math.random() * 12) + 1)
     }
-    //check if array contains at least one odd number
-    if (assign.includes(1,3,5,7,9,11)) {
-    var gem1 = assign[0]  
-    var gem2 = assign[1]  
-    var gem3 = assign[2]  
-    var gem4 = assign[3] 
-    } else {
-        assign = []
-        for(i = 0; i < 4; i++) {
-            assign.push(Math.floor(Math.random() * 12) + 1)
-        }
-    }
-    console.log(assign)
-} 
-
-console.log(randomGem())
+}
+//check if array contains at least one odd number
+// function odd () {
+//     if (assign.includes(1,3,5,7,9,11)) {
+//         var gem1 = assign[0]  
+//         var gem2 = assign[1]  
+//         var gem3 = assign[2]  
+//         var gem4 = assign[3] 
+//     } else {
+//         assign = []
+//         for(i = 0; i < 4; i++) {
+//             assign.push(Math.floor(Math.random() * 12) + 1)
+//         }
+//     }
+// } 
 
 //Reset function
 function reset() {
     randomNumber
-    randomGem
+    game()
     totalScore = 0;
 }
 
-//Game function
+
+randomGem()
+
+// click events
+$('#gem-1').on("click", function() {totalScore += assign[0]; $('#total-score').html(totalScore)}); 
+$('#gem-2').on("click", function() {totalScore += assign[1]; $('#total-score').html(totalScore)}); 
+$('#gem-3').on("click", function() {totalScore += assign[2]; $('#total-score').html(totalScore)}); 
+$('#gem-4').on("click", function() {totalScore += assign[3]; $('#total-score').html(totalScore)}); 
+
+    
