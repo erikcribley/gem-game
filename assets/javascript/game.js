e6b8a938f81501567f2428ab22b9f550635e0c73
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-01 21:00:20 -0400

    added if statement to ensure at least 1 odd number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,15 +11,25 @@ $("#random-number").html(randomNumber)
 //Generate random gem values
 function randomGem () {
     var assign = []
-    var odd = [1, 3, 5, 7, 9, 11]
     for(i = 0; i < 4; i++) {
         assign.push(Math.floor(Math.random() * 12) + 1)
     }
+    //check if array contains at least one odd number
+    if (assign.includes(1,3,5,7,9,11)) {
     var gem1 = assign[0]  
     var gem2 = assign[1]  
     var gem3 = assign[2]  
     var gem4 = assign[3] 
+    } else {
+        assign = []
+        for(i = 0; i < 4; i++) {
+            assign.push(Math.floor(Math.random() * 12) + 1)
+        }
+    }
+    console.log(assign)
 } 
+
+console.log(randomGem())
 
 //Reset function
 function reset() {
