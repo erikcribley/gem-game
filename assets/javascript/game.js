a75dcaf532de062bcde88c84bf62367a4019b77c
Erik Cribley <49967226+erikcribley@users.noreply.github.com>
2019-06-02 14:38:49 -0400

    properly coded click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 //Assign global variables
 var wins = 0;
 var losses = 0;
-var totalScore = 0;
 var assign = []
+
+$("#wins").html(wins)
+$("#losses").html(losses)
 
 //Generate random number
 var randomNumber = (Math.floor(Math.random() * 101)+ 19)
@@ -10,8 +12,7 @@ var randomNumber = (Math.floor(Math.random() * 101)+ 19)
 //Display in random-number div
 var displayRandom = $("#random-number").text(randomNumber)
 
-//Display total score in total-score div
-var displayScore = $('#total-score').html(totalScore)
+
 
 //Generate random gem values
 function randomGem () {
@@ -19,6 +20,7 @@ function randomGem () {
         assign.push(Math.floor(Math.random() * 12) + 1)
     }
 }
+
 //check if array contains at least one odd number
 // function odd () {
 //     if (assign.includes(1,3,5,7,9,11)) {
@@ -37,17 +39,67 @@ function randomGem () {
 //Reset function
 function reset() {
     randomNumber
-    game()
-    totalScore = 0;
+    assign = []
+    totalScore = 0
+    randomGem()
 }
+
+totalScore = 0;
+
+//Win-Loss conditions
+function gameOver () {
+    if (totalScore === randomNumber) {
+        wins++;
+        $("#wins").html(wins)
+        reset()
+    } else if (totalScore > randomNumber) {
+        losses++;
+
+        reset()
+    }
+    }
+
+//Display total score in total-score div
+$('#total-score').html(totalScore);
+
+$('#gem-1').on("click", function () {
+    totalScore += (assign[0]);
+    $('#total-score').html(totalScore);
+    gameOver()   
+}); 
+$('#gem-2').on("click", function() {
+    totalScore += (assign[1]);
+    $('#total-score').html(totalScore);
+    gameOver()
+}); 
+$('#gem-3').on("click", function() {
+    totalScore += (assign[2]);
+    $('#total-score').html(totalScore);
+    gameOver()
+}); 
+$('#gem-4').on("click", function() {
+    totalScore += (assign[3]);   
+    $('#total-score').html(totalScore);
+    gameOver()
+}); 
 
 
 randomGem()
 
 // click events
-$('#gem-1').on("click", function() {totalScore += assign[0]; $('#total-score').html(totalScore)}); 
-$('#gem-2').on("click", function() {totalScore += assign[1]; $('#total-score').html(totalScore)}); 
-$('#gem-3').on("click", function() {totalScore += assign[2]; $('#total-score').html(totalScore)}); 
-$('#gem-4').on("click", function() {totalScore += assign[3]; $('#total-score').html(totalScore)}); 
 
-    
+
+//Win-Loss conditions
+function gameOver () {
+if (totalScore === randomNumber) {
+    wins++;
+    reset()
+} else if (totalScore > randomNumber) {
+    losses++;
+    reset()
+}
+}
+
+console.log(assign)
+console.log(totalScore)
+
